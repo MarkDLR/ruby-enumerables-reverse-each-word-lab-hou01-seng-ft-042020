9b2c57c64469f3677e72020226d2952c6bdca34b
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-08 06:39:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split("")
-  p sentence_array
+  #sentence_array = sentence.split("")
+  #p sentence_array
+  #reverse_each_word(string)
+  string_array = string.split(/ /)
+  reversed = string_array.collect { |s|
+    s.reverse
+  }
+  reversed.join(" ")
 end
